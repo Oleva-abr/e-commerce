aa2857627b0a4699e40e9035f2abdf3e66ee3513
Olena <72007221+Oleva-abr@users.noreply.github.com>
2023-12-05 14:05:12 +0200

    created footer

--- src/app/_components/Header/MobileNav/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Header } from '../../../../payload/payload-types'
-
-function MobileNav({ header }: { header: Header }) {
-  return <div>MobileNav</div>
-}
-
-export default MobileNav
